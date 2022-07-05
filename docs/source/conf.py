a7d9dedda4239d6508ca9cacb11da70a58c774f1
wankaidi0 <108682516+wankaidi0@users.noreply.github.com>
2022-07-05 18:23:49 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ANN_guide'
+copyright = '2022, Kaidi'
+author = 'Kaidi'
 
 release = '0.1'
 version = '0.1.0'
